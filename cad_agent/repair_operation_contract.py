f4b956299f37ad83ea2dabc2f4da85298e094a8e
dkv <dkv@3407d-vr.local>
2026-08-12 02:43:01 +0700

    style: remove unused repair contract import

--- cad_agent/repair_operation_contract.py
+++ cad_agent/repair_operation_contract.py
@@ -13,7 +13,6 @@ import re
 from collections.abc import Mapping
 from dataclasses import dataclass
 from types import MappingProxyType
-from typing import Any
 
 
 REPAIR_OPERATION_SCHEMA_VERSION = "repair-operation-1.0"
